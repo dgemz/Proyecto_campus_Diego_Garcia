3da7295a027145c35dc008e69cbfa74a4df40a83
Angelinic05 <152038826+Angelinic05@users.noreply.github.com>
2024-02-15 17:27:01 -0500

    2 funciones nuevas

--- Sistema_prin.py
+++ Sistema_prin.py
@@ -12,17 +12,17 @@ def ingreso_campers(id, nombres, apellidos, direccion, acudiente, telefonoc, tel
     }
 
 def aprobar_camper(id, nota_teorica, nota_practica):
-
-    if id in camper:
+    if id in camper.keys():
         nota_prueba=(nota_practica+nota_teorica)/2
         if nota_prueba>=60:
             camper[id]['Estado'] = 'Aprobado'
         return camper
     else:
         return "No se encontró el camper con el id: " + str(id)
-    
+
 ruta={}
 salon={}
+notas={}
 
 ruta['Java']={
     'Modulo 1': 'Introduccón a Java',
@@ -30,7 +30,6 @@ ruta['Java']={
     'Modulo 3': 'Especialización y uso real de Java'
 }
 salon['Java']= {'Trainer': {'No asignado'}}
-
 
 ruta['NetCore']={
     'Modulo 1': 'Introduccón a NetCore',
@@ -59,13 +58,14 @@ def crear_ruta(nombre_ruta):
 trainers={}
 
 def ingresar_trainer(nombre_trainer, ruta_trainer):
-    if not nombre_trainer in salon:
+    if not nombre_trainer in salon.keys():
         trainers[nombre_trainer] = ruta_trainer
         salon[ruta_trainer]['Trainer'] = {nombre_trainer}
 
 def asignar_ruta(id_camper, ruta_asignada):
-    if ruta_asignada in ruta:
-        if id_camper in camper:
+    print(ruta)
+    if ruta_asignada in ruta.keys():
+        if id_camper in camper.keys():
             if camper[id_camper]['Estado']=='Aprobado':
                 num_est = len(salon[ruta_asignada])
                 if num_est < 33:
@@ -78,5 +78,26 @@ def asignar_ruta(id_camper, ruta_asignada):
         else:
             print("La identificación no está en el sistema.")
     else:
-        print("La ruta no existe. ")
+        print("La ruta no existe o aun no tiene un TRAINER designado. ")
 
+def calc_notas(id_camper, ruta_cam, modulo, nota_proyect, nota_quiz, nota_trainer):
+    if ruta_cam in ruta.keys():
+        if id_camper in camper.keys():
+            if camper[id_camper]['Estado']=='Aprobado':
+                num_est = len(salon[ruta_asignada])
+                if num_est < 33:
+                    estudiante = f"Estudiante {num_est}"
+                    salon[ruta_asignada][estudiante] = camper[id_camper]
+                else:
+                    print("La ruta está llena.")
+            else:
+                print("El alumno no esta aprobado. ")
+        else:
+            print("La identificación no está en el sistema.")
+    else:
+        print("La ruta no existe o aun no tiene un TRAINER designado. ")
+        if modulo in ruta.keys():
+            nota=(nota_proyect*0.6)+(nota_quiz*0.3)+(nota_trainer*0.1)
+            notas[id_camper]={modulo: nota}
+        else:
+            print("El modulo no existe")
